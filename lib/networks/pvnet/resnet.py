79e235fe77aacfb2a4160804ee3c6ff26c4446af
arturo <artguridi@gmail.com>
2021-10-01 16:27:44 +0200

    Changed requirements.txt and small comments

--- lib/networks/pvnet/resnet.py
+++ lib/networks/pvnet/resnet.py
@@ -134,6 +134,7 @@ class ResNet(nn.Module):
         self.inplanes = 64
         self.fully_conv = fully_conv
         super(ResNet, self).__init__()
+        # modify here the number of input channels
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
                                bias=False)
         self.bn1 = nn.BatchNorm2d(64)
