f38e2ee8964a20ea928f22b9d9d6aa68aacd6e03
ge93sut <ge93sut@mytum.de>
2021-11-26 00:10:35 +0100

    Added graph writer 3

--- lib/train/recorder.py
+++ lib/train/recorder.py
@@ -61,7 +61,10 @@ class Recorder(object):
 
     def add_net(self, network, images_loader):
         dataiter = iter(images_loader)
-        im = dataiter.next()['inp']
+        batch = dataiter.next()
+        for k in batch:
+            if k != 'meta':     # Facebook, meta and cuda
+                batch[k] = batch[k].cuda()
         network.eval()
         self.writer.add_graph(network, im)
 
